4fb2b843a904da3d9b8e58f450e27277dcd0132b
adeline <lala.elgalta@hotmail.fr>
2022-12-20 18:13:40 +0100

    creation du component devant contenir les jours + heures

---
+++ frontend/Ryan_hair_front/src/components/HomeTable.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+export default function HomeTable() {
+
+    return (
+        <div id="table" className='table'>
+
+
+
+        </div>
+    );
+}
