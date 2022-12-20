3f458425b9f9da92ed5b4a28ad403bd2c1c9b61e
Fang <fang@mi2n.net>
2022-12-20 14:52:09 +0100

    Routes

--- frontend/Ryan_hair_front/src/main.tsx
+++ frontend/Ryan_hair_front/src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        //element:
+
+    },
+    {
+        path: "rendezvous",
+        //element:
+    },
+    {
+        path:"admin"
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
