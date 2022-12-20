6cd0d162c3191df77cc1fc2154c493ef7ad09296
adeline <lala.elgalta@hotmail.fr>
2022-12-20 18:11:45 +0100

    creation de components + route

---
+++ frontend/Ryan_hair_front/src/components/Home.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+
+
+
+    return (
+        <div id="home" className='home'>
+
+            <h2 className="homeTitrePresentation">
+                Des cheveux en plein envol
+            </h2>
+
+            <p className="homePremierParagraphe">
+                Bienvenue chez Ryan Hair, où les cheveux sont rois!
+            </p>
+
+            <p className="homeSecondParagraphe">
+                Je suis Ryan, coiffeur passionné et talentueux, prêt à tresser la magie dans vos cheveux.
+                Que vous ayez envie d'une coupe de cheveux fraîche, d'une coloration envoûtante, ou simplement de prendre
+                soin de vous, mon salon est l'endroit idéal pour vous.
+            </p>
+
+            <p className="homeTroisiemeParagraphe">
+                Mon équipement de pointe et mon atmosphère accueillante vous feront sentir comme sur un nuage.
+                Et grâce à mon emplacement central et mes heures d'ouverture du mardi au samedi de 9h30 à 18h00,
+                vous pourrez facilement planifier votre rendez-vous quand cela vous convient le mieux.
+            </p>
+
+            <p className="homeQuatriemeParagraphe">
+                Alors n'hésitez pas, et laissez-moi coiffer vos rêves! Prenez rendez-vous dès maintenant et
+                découvrez la magie de Ryan Hair.
+            </p>
+
+        </div>
+    );
+}
--- frontend/Ryan_hair_front/src/main.tsx
+++ frontend/Ryan_hair_front/src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Home from "./components/Home";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        //element:
+        element : <Home />
 
     },
     {
