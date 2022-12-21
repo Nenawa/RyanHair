818e7ee4ac7193c50d0165029d486e6f93da491a
adeline <lala.elgalta@hotmail.fr>
2022-12-21 13:58:02 +0100

    mise à jour

--- frontend/Ryan_hair_front/src/main.tsx
+++ frontend/Ryan_hair_front/src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Home from "./pages/Home";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element : <Home />
-
     },
     {
         path: "rendezvous",
