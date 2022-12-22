f38b4f79c484cb780f41108b6fb661c43df8c129
adeline <lala.elgalta@hotmail.fr>
2022-12-22 15:19:12 +0100

    correction de nommage dans Presentation.txt + debut de table

--- frontend/Ryan_hair_front/src/components/home/Table.tsx
+++ frontend/Ryan_hair_front/src/components/home/Table.tsx
@@ -16,20 +16,8 @@ export default function Table() {
 
     useEffect(() => {
         API_URL =
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=agenda-des-manifestations-culturelles-so-toulouse&q=&rows=50&start=" +
-            indexAPI +
-            "&facet=date_debut&facet=date_fin&facet=categorie_de_la_manifestation&facet=theme_de_la_manifestation&refine.date_debut=" +
-            currentYear +
-            "-" +
-            currentMonth +
-            "&refine.date_fin=" +
-            currentYearAfter30Days +
-            "-" +
-            currentMonthAfter30days;
-        axios.get(API_URL).then((res) => {
-            const occurrence = res.data;
-            setOccurrences(occurrence);
-        });
+            ""
+
     }, [indexAPI]);
 
 
