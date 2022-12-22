999acf045103ce8f774f8cfb3c0f5e4ad22f2c62
Anne W <anne.walch@yahoo.fr>
2022-12-22 19:35:46 +0100

    Poursuite CSS navbar

--- frontend/Ryan_hair_front/src/components/Navbar.tsx
+++ frontend/Ryan_hair_front/src/components/Navbar.tsx
@@ -6,24 +6,25 @@ export default function Navbar() {
         <>
             <div className="navbar flex">
                 <img className="navbar-logo" src="src/assets/images/logo.png" alt="logo Ryan Hair" />
-                <ul className="navbar-list flex ">
-                    <li>
-                        <Link to="/" >accueil</Link>
-                    </li>
-                    <li>
-                        <Link to="/" >homme</Link>
-                    </li>
-                    <li>
-                        <Link to="/" >femme</Link>
-                    </li>
-                    <li>
-                        <Link to="/rendezvous" >rendez-vous</Link>
-                    </li>
-                </ul>
-                <div>
-                
+                <div className="navbar-background">
+                    <ul className="navbar-list flex ">
+                        <li>
+                            <Link to="/" >accueil</Link>
+                        </li>
+                        <li>
+                            <Link to="/" >homme</Link>
+                        </li>
+                        <li>
+                            <Link to="/" >femme</Link>
+                        </li>
+                        <li>
+                            <Link to="/rendezvous" >rendez-vous</Link>
+                        </li>
+                    </ul>
+
+                    <div className="admin">
                         <Link to="/admin" >administration</Link>
-                  
+                    </div>
                 </div>
             </div>
         </>
