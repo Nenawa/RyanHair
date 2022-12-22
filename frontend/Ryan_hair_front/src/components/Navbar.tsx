c6cefe145f76390356cae700a16b2c833541bd86
Anne W <anne.walch@yahoo.fr>
2022-12-22 18:49:35 +0100

    Début css Navbar

--- frontend/Ryan_hair_front/src/components/Navbar.tsx
+++ frontend/Ryan_hair_front/src/components/Navbar.tsx
@@ -1,26 +1,31 @@
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 export default function Navbar() {
     return (
         <>
-        
-            <ul className="navigation">
-                <li>
-                    <Link to="/" >accueil</Link>
-                </li>
-                <li>
-                    <Link to="/" >homme</Link>
-                </li>
-                <li>
-                    <Link to="/" >femme</Link>
-                </li>
-                <li>
-                    <Link to="/rendezvous" >rendez-vous</Link>
-                </li>
-                <li>
-                    <Link to="/admin" >administration</Link>
-                </li>
-            </ul>
+            <div className="navbar flex">
+                <img className="navbar-logo" src="src/assets/images/logo.png" alt="logo Ryan Hair" />
+                <ul className="navbar-list flex ">
+                    <li>
+                        <Link to="/" >accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/" >homme</Link>
+                    </li>
+                    <li>
+                        <Link to="/" >femme</Link>
+                    </li>
+                    <li>
+                        <Link to="/rendezvous" >rendez-vous</Link>
+                    </li>
+                </ul>
+                <div>
+                
+                        <Link to="/admin" >administration</Link>
+                  
+                </div>
+            </div>
         </>
     );
 }
